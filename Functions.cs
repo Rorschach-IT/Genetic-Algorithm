f0285b7337293db08a87a21ff51f09600e156b4f
JakubP <j.poczatekrorschach@gmail.com>
2025-03-02 01:11:14 +0100

    algorithm created with no input options for user

---
+++ Functions.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Genetic_Algorithm
+{
+    internal class Functions
+    {
+        // Return number of bits (1) in array
+        public static int CalculateFitness(int[] subject)
+        {
+            int fitnessScore = 0;
+
+            foreach (var gene in subject)
+            {
+                if (gene == 1)
+                {
+                    fitnessScore++;
+                }
+            }
+
+            return fitnessScore;
+        }
+
+        // Calculating average fitness will give a better picture or population as a whole improves:
+        public static double CalculateAverageFitness(int[][] population)
+        {
+            double totalFitness = 0;
+
+            foreach (var individual in population)
+            {
+                totalFitness += CalculateFitness(individual);
+            }
+
+            return (totalFitness / population.Length);
+        }
+
+        // Selecting better parent
+        public static int[] SelectBetterParent(int[] parent1, int[] parent2)
+        {
+            int parent1Fitness = CalculateFitness(parent1);
+            int parent2Fitness = CalculateFitness(parent2);
+
+            if (parent1Fitness >= parent2Fitness)
+            {
+                return parent1;
+            }
+            else
+            {
+                return parent2;
+            }
+        }
+
+        // Selection of parents
+        public static int[] ReturnParent(int[][] population, Random random)
+        {
+            int[] candidate1 = population[random.Next(population.Length)];
+            int[] candidate2 = population[random.Next(population.Length)];
+
+            return SelectBetterParent(candidate1, candidate2);
+        }
+
+        // Creating child from mixed parents
+        public static int[] CreateChild(int[] parent1, int[] parent2, Random random)
+        {
+            int length = parent1.Length;
+            int crossoverPoint = random.Next(1, length - 1);
+
+            int[] child = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i < crossoverPoint)
+                {
+                    child[i] = parent1[i];
+                }
+                else
+                {
+                    child[i] = parent2[i];
+                }
+            }
+
+            return child;
+        }
+
+        // Mutation of the child
+        public static void Mutate(int[] individual, Random random)
+        {
+            const double mutationRate = 0.05; // 5%
+
+            for (int i = 0; i < individual.Length; i++)
+            {
+                if (random.NextDouble() < mutationRate)
+                {
+                    // Flipping the bit
+                    if (individual[i] == 0)
+                    {
+                        individual[i] = 1;
+                    }
+                    else
+                    {
+                        individual[i] = 0;
+                    }
+                }
+            }
+        }
+
+        // Function to check how many different individuals are in the population
+        public static int CountUniqueIndividuals(int[][] population)
+        {
+            HashSet<string> uniqueIndividuals = new HashSet<string>();
+
+            foreach (var individual in population)
+            {
+                string dna = string.Join("", individual);
+                uniqueIndividuals.Add(dna);
+            }
+
+            return uniqueIndividuals.Count;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Linq;
+
+namespace Genetic_Algorithm
+{
+    internal class Program
+    {
+        static void PrintPopulation(int[][] population)
+        {
+            for (int i = 0; i < population.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(string.Join("", population[i]));
+                Console.ResetColor();
+
+                Console.Write(" | Fitness --> ");
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(Functions.CalculateFitness(population[i]));
+                Console.ResetColor();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            const int numberOfPositions = 10; // The number of genes in the individual
+            const int numberOfSubjects = 8; // Number of individuals in the population
+            const int iterationCounter = 6; // Number of full algorithm iterations
+
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Running Genetic Algorithm with:");
+            Console.WriteLine($"Population size: {numberOfSubjects}");
+            Console.WriteLine($"Gene length: {numberOfPositions}");
+            Console.WriteLine($"Iterations: {iterationCounter}");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+            Console.ResetColor();
+
+            // Population initialization - each individual is a table with values: {0 or 1}
+            int[][] population = new int[numberOfSubjects][];
+            for (int i = 0; i < population.Length; i++)
+            {
+                population[i] = new int[numberOfPositions];
+                for (int j = 0; j < numberOfPositions; j++)
+                {
+                    population[i][j] = random.Next(2);
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("First population:");
+            Console.ResetColor();
+            PrintPopulation(population);
+
+            /* 
+                Main algorithm loop
+            */
+            for (int i = 0; i < iterationCounter; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"\nIteration number: {i + 1}, new population: ");
+                Console.ResetColor();
+
+                int[][] newPopulation = new int[numberOfSubjects][];
+
+                double averageFitness = Functions.CalculateAverageFitness(population);
+                int uniqueIndividuals = Functions.CountUniqueIndividuals(population);
+
+                Console.Write("Average fitness: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"{averageFitness:F2}");
+                Console.ResetColor();
+
+                Console.Write("Unique individuals: ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine(uniqueIndividuals);
+                Console.ResetColor();
+
+                // Optional elitism - maintaining the best individual
+                int[] bestIndividual = population.OrderByDescending(Functions.CalculateFitness).First();
+                newPopulation[0] = (int[])bestIndividual.Clone(); // keeping the best one
+
+                for (int j = 1; j < numberOfSubjects; j += 2)
+                {
+                    // Choosing 2 parents
+                    int[] parent1 = Functions.ReturnParent(population, random);
+                    int[] parent2 = Functions.ReturnParent(population, random);
+
+                    // Crossing (creating two children)
+                    int[] child1 = Functions.CreateChild(parent1, parent2, random);
+                    int[] child2 = Functions.CreateChild(parent1, parent2, random);
+
+                    // Mutation of children
+                    Functions.Mutate(child1, random);
+                    Functions.Mutate(child2, random);
+
+                    // Inserting children in a new population
+                    newPopulation[j] = child1;
+                    if (j + 1 < numberOfSubjects)
+                    {
+                        newPopulation[j + 1] = child2;
+                    }
+                }
+
+                // Replacing the old population with new one
+                population = newPopulation;
+
+                PrintPopulation(population);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n\nEnd of algorithm, press any key.");
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+    TO DO:
+    - add inputs for main three values with validation
+*/
